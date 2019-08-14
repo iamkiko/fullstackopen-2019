6315aed879dd905f40ade57598dd6ea673176a7f
Christos Constantinides <constantinides.ch@gmail.com>
2019-08-14 11:45:00 +0200

    3.7 - added morgan middleware for HTTP request logging

--- part3/phonebook-backend/index.js
+++ part3/phonebook-backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
+const morgan = require('morgan')
 
+app.use(morgan('tiny'))
 app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.urlencoded({ extended: false }));
     let persons = [
       {
         name: "Bill Gates",
