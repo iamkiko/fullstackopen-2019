1c6ee0219d67e2b6cc93cd287c14610c039fcaf8
Christos Constantinides <constantinides.ch@gmail.com>
2019-08-16 16:40:33 +0200

    removed backend notes (part 3) as submodule and recommitted

---
+++ part3/notes/build/precache-manifest.08432314cf498651a32e647dc624209b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "293bbe7fc10ad54a7e45ce75ce8e9852",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f568647bfd9823c2cede",
+    "url": "/static/css/main.075049df.chunk.css"
+  },
+  {
+    "revision": "1af2e68c30103fa82895",
+    "url": "/static/js/2.f3384baa.chunk.js"
+  },
+  {
+    "revision": "f568647bfd9823c2cede",
+    "url": "/static/js/main.e34f90f9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
