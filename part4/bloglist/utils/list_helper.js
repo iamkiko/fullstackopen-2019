746915502173690334a782bb85bb019994403333
Christos <constantinides.ch@gmail.com>
2019-09-09 16:17:02 +0200

    refactored list_helper.js

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -4,13 +4,11 @@ const dummy = (blogs) => {
   
 const totalLikes = blogs => blogs.reduce((sum, { likes }) => sum + likes, 0) //need to destructure to avoid [object, object] error
 
-const favoriteBlog = blogs => {
-    return blogs.reduce((a , b) => {
+const favoriteBlog = blogs =>  blogs.reduce((a , b) => {
         return a.likes > b.likes
         ? {title: a.title, author: a.author, likes: a.likes}
         : {title: b.title, author: b.author, likes: b.likes}
     })
-}
 
 module.exports = {
     dummy,
