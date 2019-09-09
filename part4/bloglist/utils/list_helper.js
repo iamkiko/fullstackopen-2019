4f7f710c0268e47c29084f88f712d319c00bb35f
Christos <constantinides.ch@gmail.com>
2019-09-09 16:15:29 +0200

    created jest test cases for 4.3 - 4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -2,6 +2,19 @@ const dummy = (blogs) => {
    return 1
   }
   
-  module.exports = {
-    dummy
+const totalLikes = blogs => blogs.reduce((sum, { likes }) => sum + likes, 0) //need to destructure to avoid [object, object] error
+
+const favoriteBlog = blogs => {
+    return blogs.reduce((a , b) => {
+        return a.likes > b.likes
+        ? {title: a.title, author: a.author, likes: a.likes}
+        : {title: b.title, author: b.author, likes: b.likes}
+    })
+}
+
+module.exports = {
+    dummy,
+    totalLikes,
+    favoriteBlog
   }
+  
