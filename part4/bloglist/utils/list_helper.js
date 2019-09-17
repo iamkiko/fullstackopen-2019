a83f01f362e7467a5ea3c4fec49f05f1c9398808
Christos <constantinides.ch@gmail.com>
2019-09-17 09:00:26 +0200

    4.7 - added mostLikes and mostBlogs to list_helper.js

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -10,9 +10,55 @@ const favoriteBlog = blogs =>  blogs.reduce((a , b) => {
         : {title: b.title, author: b.author, likes: b.likes}
     })
 
+const mostBlogs = blogs => {
+  let sum = {}
+  let check = 0;
+  let highest;
+
+  blogs.forEach((blog, i) => {
+    const author = blogs[i].author
+    if (sum[author] === undefined) {
+      sum[author] = 1
+    } else {
+      sum[author] = sum[author]++
+    }
+
+    if (sum[author] > check) {
+      check = sum[author]
+      highest = blogs[i]
+    }
+  })
+
+  return {
+    author: highest.author,
+    blogs: check //number of blogs
+  }
+}
+
+const mostLikes = blogs => {
+  let totalLikes = 0
+
+  const mostPopular = blogs.reduce((a, b) => a.totalLikes > b.totalLikes
+    ? a
+    : b 
+  )
+
+  blogs.forEach(blog => blog.author === mostPopular.author
+    ? totalLikes += blog.totalLikes
+    : 0
+  )
+
+  return {
+    author: mostPopular.author,
+    totalLikes: totalLikes
+  }
+}
+
 module.exports = {
     dummy,
     totalLikes,
-    favoriteBlog
+    favoriteBlog,
+    mostBlogs,
+    mostLikes
   }
   
