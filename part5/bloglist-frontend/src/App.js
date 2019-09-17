346571439a61be15828c18367b1040a1fa852453
Christos <constantinides.ch@gmail.com>
2019-09-17 10:47:46 +0200

    5.1 - started bloglist-frontend + added conditional login page

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -62,30 +62,6 @@ const App = () => {
     />
     )
 
-//   const loginForm = () => (
-//       <form onSubmit={handleLogin}>
-//           <div>
-//               username: 
-//               <input
-//               type="text"
-//               value={username}
-//               name="Username"
-//               onChange={({target}) => setUsername(target.value)}
-//               />
-//           </div>
-//           <div>
-//               password:
-//               <input
-//               type="password"
-//               value={password}
-//               name="Password"
-//               onChange={({target}) => setPassword(target.value)}
-//               />
-//           </div>
-//           <button type="submit">Login!</button>
-//       </form>
-//   )
-
   //need to add blog form to submit (like noteForm)
 
 //   const handleBlogChange = (event) => {
