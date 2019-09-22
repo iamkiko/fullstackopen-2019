15ed6350ca40c506c4911a6a7ef92c8015e71167
Christos <constantinides.ch@gmail.com>
2019-09-22 10:35:24 +0200

    5.14 - wrote test to verify that if like button pressed twice, the event handler fn in simpleBlog.js props is called twice

--- part5/bloglist-frontend/src/components/SimpleBlog.test.js
+++ part5/bloglist-frontend/src/components/SimpleBlog.test.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { render, fireEvent, prettyDOM } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import '@testing-library/jest-dom/extend-expect'
 // import { prettyDOM } from '@testing-library/dom'
 import SimpleBlog from './SimpleBlog'
 
 describe('Simpleblog tests', () => {
-
-
   test('renders title, author and likes', () => {
 
     const blog = {
@@ -24,9 +22,25 @@ describe('Simpleblog tests', () => {
 
     const likes = component.container.querySelector('.likes')
     expect(likes).toHaveTextContent(12)
-
   })
 
-  
+  test('clicking the like button calls event handler twice', () => {
+    const blog = {
+      title: 'testing react frontend',
+      author: 'Christos',
+      likes: 12
+    }
 
+    const mockHandler = jest.fn()
+
+    const { getByText } = render(
+      <SimpleBlog blog={blog} onClick={mockHandler}/>
+    )
+
+    const button = getByText('like')
+    fireEvent.click(button)
+    fireEvent.click(button)
+
+    expect(mockHandler.mock.calls.length).toBe(2)
+  })
 })
