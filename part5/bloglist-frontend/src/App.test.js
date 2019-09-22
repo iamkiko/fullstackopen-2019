9408b7d68f1e35e692400c3d3a39f62f9bfcf997
Christos <constantinides.ch@gmail.com>
2019-09-22 15:12:16 +0200

    5.17 - wrote test to verify that when a user is logged in - 2 blog posts rendered - with localstorage etc.

--- part5/bloglist-frontend/src/App.test.js
+++ part5/bloglist-frontend/src/App.test.js
@@ -1,11 +1,12 @@
 import React from "react"
 import {
-  render, waitForElement
+  render, waitForElement, cleanup
 } from "@testing-library/react"
 jest.mock("./services/blogs")
 import App from "./App"
 
 describe("<App />", () => {
+  afterEach(cleanup)
   test("if no user logged, blogs are not rendered", async () => {
     const component = render(
       <App />
@@ -17,9 +18,28 @@ describe("<App />", () => {
     )
 
     // expectations here
-    const notLoggedInBlogs = component.container.querySelectorAll(".blogInfo")
+    const notLoggedInBlogs = component.container.querySelectorAll(".basicInfo")
 
     expect (notLoggedInBlogs.length).toBe(0)
 
+  })
+
+  test("blogs appear when user is logged in", async () => {
+    const user = {
+      username: "MR TEST",
+      name: "TEST",
+      token: 1231231214
+    }
+
+    localStorage.setItem("loggedBlogUser", JSON.stringify(user))
+    const component =  render(<App/>)
+    component.rerender(<App/>)
+
+    await waitForElement(
+      () => component.getByText("Logout")
+    )
+
+    const loggedInBlogs = component.container.querySelectorAll(".basicInfo")
+    expect(loggedInBlogs.length).toBe(2)
   })
 })
