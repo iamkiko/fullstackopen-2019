6f66183ed895af14eea46e4f0766aa9cd4a890db
Christos <constantinides.ch@gmail.com>
2019-12-27 15:42:08 +0200

    8.3 - implemented query allAuthors for details of authors + added bookCount to Authors resolver

--- part8/exercises/index.js
+++ part8/exercises/index.js
@@ -93,10 +93,18 @@ const typeDefs = gql`
     genres: [String!]!
   }
 
+  type Author {
+    name: String!
+    id: ID!
+    born: Int
+    bookCount: Int!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
     allBooks: [Book!]!
+    allAuthors: [Author!]!
   }
 `;
 
@@ -104,7 +112,14 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: () => books
+    allBooks: () => books,
+    allAuthors: () => authors
+  },
+  Author: {
+    bookCount: root => {
+      const authoredBooks = books.filter(book => book.author === root.name);
+      return authoredBooks.length;
+    }
   }
 };
 
