5f9ff4ac542076f2d04b087abe506b6f99ce5384
Christos <constantinides.ch@gmail.com>
2019-12-27 15:10:56 +0200

    8.2 - updated queries to return allBooks

--- part8/exercises/index.js
+++ part8/exercises/index.js
@@ -85,16 +85,26 @@ let books = [
 ];
 
 const typeDefs = gql`
+  type Book {
+    title: String!
+    published: Int!
+    author: String!
+    id: ID!
+    genres: [String!]!
+  }
+
   type Query {
     bookCount: Int!
     authorCount: Int!
+    allBooks: [Book!]!
   }
 `;
 
 const resolvers = {
   Query: {
     bookCount: () => books.length,
-    authorCount: () => authors.length
+    authorCount: () => authors.length,
+    allBooks: () => books
   }
 };
 
